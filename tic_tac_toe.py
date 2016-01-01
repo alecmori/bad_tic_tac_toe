d97aa9e286146a255b53fd0579a278b1e4317fb5
Alec Mori <alecmori@gmail.com>
2015-12-31 20:26:14 -0600

    Updated code - incorrect earlier

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a8d7d1251c2c033cb43066cdb7c9e6e8a75ffc4f7b640e460e98030faaae079
-size 171147886
+oid sha256:5a1abef4596fa3c4ede158c52b053240539beaf1b3a586a55fe4e72946867c21
+size 156703764
